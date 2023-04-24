19fb5985f0e6a81b06343b734715b0573f003fdf
Jampiergit <129233054+Jampiergit@users.noreply.github.com>
2023-04-23 23:20:30 -0500

    Add files via upload

---
+++ SQLQuery5.sql
@@ -0,0 +1,14 @@
+use naturvida1
+
+
+
+exec SP_BUSCARINVENTARIO
+
+exec SP_DESCONTARCANT
+
+exec  SP_BUSCARINVENTARIOS
+
+select * from Productos
+
+
+alter authorization on database:: NaturVida1 to SA
